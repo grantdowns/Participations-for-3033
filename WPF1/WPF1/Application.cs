e3381645541058c570d2a74b7fadda95ea1d8e81
down8427 <down8427@PCB-LAB360-13>
2019-02-06 11:35:58 -0600

    First Participation Done

---
+++ WPF1/WPF1/Application.cs
@@ -0,0 +1,36 @@
+﻿//Grant Downs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    public class Application1
+    {
+        public string Name;
+        public string Address;
+        public int ZipCode;
+
+        public Application1()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+
+        }
+
+        public Application1(string name, string address, int zipCode)
+        {
+            name = Name;
+            address = Address;
+            zipCode = ZipCode;
+
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Address} {ZipCode}";
+        }
+    }
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsubmit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Application1 Person = new Application1();
+            Person.Name = txtname.Text;
+            //i changed these names but theyre still from when I created them?
+            Person.Address = txtaddress.Text;
+            Person.ZipCode = int.Parse(txtzip.Text);
+            Person.ToString();
+            //cant add to listbox
+            lstaddress.Items.Add(Person);
+
+
+        }
+    }
+}
